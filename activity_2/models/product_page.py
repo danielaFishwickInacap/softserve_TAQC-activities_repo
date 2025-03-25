f6d8387309c9cb2b4c1f702bb97885ec3ccfe894
Daniela <daniela.fishwick@inacapmail.cl>
2025-03-25 12:33:01 -0300

    edit: product_page.py

--- activity_2/models/product_page.py
+++ activity_2/models/product_page.py
@@ -3,7 +3,6 @@ from playwright.sync_api import Page
 class Product_page():
     def __init__(self, page : Page):
         self.page = page
-        self.container = page.locator("div[data-test='inventory-item']")
         self.add_to_cart_button = page.locator("button[data-test='add-to-cart']")
         self.shopping_cart_link = page.locator("a[data-test='shopping-cart-link']")
 
